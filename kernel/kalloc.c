47677ecb23755359e66b1a0df528600c89c07363
StLeoX <1228354389@qq.com>
2021-11-23 22:01:51 +0800

    passed multi-core kalloc

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,14 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i=0; i<NCPU; i++){
+    initlock(&kmem[i].lock, "kmem");
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +58,15 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();// turn cpu-interrupt off
+
+  int i=cpuid();//get cpu id
+  acquire(&kmem[i].lock);
+  r->next = kmem[i].freelist;
+  kmem[i].freelist = r;
+  release(&kmem[i].lock);
+
+  pop_off();// turn cpu-interrupt-on
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +77,31 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+
+  int i=cpuid();
+  acquire(&kmem[i].lock);
+  r = kmem[i].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[i].freelist = r->next;
+  release(&kmem[i].lock);
+
+  if(!r){// now we should seek for available freelist from other cpu
+    for(int j=0; j<NCPU; j++){
+      if(j!=i){ // filter logic
+        acquire(&kmem[j].lock);
+        if(kmem[j].freelist){
+          r=kmem[j].freelist;
+          kmem[j].freelist=r->next;
+          release(&kmem[j].lock);
+          break;
+        }
+        release(&kmem[j].lock);//!note this
+
+      }
+    }
+  }
+  pop_off();//turn on
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
